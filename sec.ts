0fc38bc88d2d1af1bfd249415b8d0720f4cb2a44
MatheusFreitasG <131834576+MatheusFreitasG@users.noreply.github.com>
2023-11-24 11:33:23 -0300

    Final

---
+++ sec.ts
@@ -0,0 +1,25 @@
+import { Biblioteca } from "./contagem";
+
+class Secao extends Biblioteca {
+    visitantes: number;
+
+    constructor(nome: string, totalVisitantes: number = 0, visitantes: number = 0){
+        super(nome, totalVisitantes);
+        this.visitantes = visitantes;
+    }
+
+    registrarVisitaSeção(){
+        this.visitantes++;
+    }
+
+    obterVisitantesSeção(){
+        return this.visitantes;
+    }
+
+}
+
+const bibliotecaSenai = new Secao("Ficção", 10);
+
+const resposta = bibliotecaSenai.obterTotalVisitantes();
+
+console.log(resposta);
